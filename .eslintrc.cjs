6349c68fd262b8d79c9f827a812765269cb99c2b
Eathyn <eathynzakas@gmail.com>
2022-08-24 23:30:51 +0800

    fix: eslint unresolved import

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,9 +6,20 @@ module.exports = {
   },
   extends: [
     'plugin:vue/vue3-essential',
+    'plugin:import/recommended',
     'airbnb-base',
     'prettier',
   ],
+  settings: {
+    // https://stackoverflow.com/a/67847853/9863318
+    'import/resolver': {
+      alias: {
+        map: [
+          ['@', './src'],
+        ],
+      },
+    },
+  },
   overrides: [
   ],
   parserOptions: {
